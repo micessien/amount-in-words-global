0ab87b12b686059525daf5aae96d475e6d29c72d
micessien <micaeldie@gmail.com>
2023-07-07 14:15:58 +0000

    Adding Ghana country code

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,68 @@
-import { Greeter } from '../index';
+import { AmountToWords, CountryCodes } from '../index';
 
-test('My Greeter', () => {
-  expect(Greeter('Carl')).toBe('Hello Carl');
+const atw = new AmountToWords();
+
+// Indian Numbering System
+test('supports Indian country code', () => {
+  expect(atw.toWords(123)).toEqual('One Hundred Twenty Three Rupees');
+  expect(atw.toWords(1000)).toEqual('One Thousand Rupees');
+  expect(atw.toWords(101010)).toEqual('One Lakh One Thousand Ten Rupees');
+  expect(atw.toWords('99,91,01,010')).toEqual('Ninety Nine Crore Ninety One Lakh One Thousand Ten Rupees');
+  expect(atw.toWords('99,91,01,010.01')).toEqual(
+    'Ninety Nine Crore Ninety One Lakh One Thousand Ten Rupees And One Paisa',
+  );
+  expect(atw.toWords('99,91,01,010.23')).toEqual(
+    'Ninety Nine Crore Ninety One Lakh One Thousand Ten Rupees And Twenty Three Paise',
+  );
+});
+
+// USA Numbering System
+test('supports USA country code', () => {
+  expect(atw.toWords(1921000, CountryCodes.USA)).toEqual('One Million Nine Hundred Twenty One Thousand Dollars');
+  expect(atw.toWords(554272561010, CountryCodes.USA)).toEqual(
+    'Five Hundred Fifty Four Billion Two Hundred Seventy Two Million Five Hundred Sixty One Thousand Ten Dollars',
+  );
+  expect(atw.toWords('0.01', CountryCodes.USA)).toEqual('Zero Dollar And One Cent');
+  expect(atw.toWords('001.87', CountryCodes.USA)).toEqual('One Dollar And Eighty Seven Cents');
+});
+
+// UK Numbering System
+test('supports GBR country code', () => {
+  expect(atw.toWords('632,362,999,101,001', CountryCodes.GBR)).toEqual(
+    'Six Hundred Thirty Two Trillion Three Hundred Sixty Two Billion Nine Hundred Ninety Nine Million One Hundred One Thousand One Pounds',
+  );
+  expect(atw.toWords('0.01', CountryCodes.GBR)).toEqual('Zero Pound And One Pence');
+  expect(atw.toWords('001.55', CountryCodes.GBR)).toEqual('One Pound And Fifty Five Pence');
+  expect(atw.toWords('100,000', CountryCodes.GBR)).toEqual('One Hundred Thousand Pounds');
+  expect(atw.toWords('100,000,000', CountryCodes.GBR)).toEqual('One Hundred Million Pounds');
+  expect(atw.toWords('10,000,000,000', CountryCodes.GBR)).toEqual('Ten Billion Pounds');
+});
+
+// Nigeria Numbering System
+test('supports NGR country code', () => {
+  expect(atw.toWords(1000, CountryCodes.NGR)).toEqual('One Thousand Naira');
+  expect(atw.toWords('001.55', CountryCodes.NGR)).toEqual('One Naira And Fifty Five Kobo');
+  expect(atw.toWords('0.01', CountryCodes.NGR)).toEqual('Zero Naira And One Kobo');
+  expect(atw.toWords('001.87', CountryCodes.NGR)).toEqual('One Naira And Eighty Seven Kobo');
+  expect(atw.toWords('632,362,999,101,001', CountryCodes.NGR)).toEqual(
+    'Six Hundred Thirty Two Trillion Three Hundred Sixty Two Billion Nine Hundred Ninety Nine Million One Hundred One Thousand One Naira',
+  );
+  expect(atw.toWords(554272561010, CountryCodes.NGR)).toEqual(
+    'Five Hundred Fifty Four Billion Two Hundred Seventy Two Million Five Hundred Sixty One Thousand Ten Naira',
+  );
+});
+
+// Ghana Numbering System
+test('supports GH country code', () => {
+  expect(atw.toWords(1921000, CountryCodes.GH)).toEqual('One Million Nine Hundred Twenty One Thousand Cedis');
+  expect(atw.toWords('001.55', CountryCodes.GH)).toEqual('One Cedis And Fifty Five Pesewas');
+  expect(atw.toWords('0.01', CountryCodes.GH)).toEqual('Zero Cedis And One Pesewas');
+  expect(atw.toWords('001.87', CountryCodes.GH)).toEqual('One Cedis And Eighty Seven Pesewas');
+  expect(atw.toWords('100,000', CountryCodes.GH)).toEqual('One Hundred Thousand Cedis');
+  expect(atw.toWords('632,362,999,101,001', CountryCodes.GH)).toEqual(
+    'Six Hundred Thirty Two Trillion Three Hundred Sixty Two Billion Nine Hundred Ninety Nine Million One Hundred One Thousand One Cedis',
+  );
+  expect(atw.toWords(554272561010, CountryCodes.GH)).toEqual(
+    'Five Hundred Fifty Four Billion Two Hundred Seventy Two Million Five Hundred Sixty One Thousand Ten Cedis',
+  );
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,146 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+// Country code
+export enum CountryCodes {
+  IND = 'IND',
+  USA = 'USA',
+  GBR = 'GBR',
+  NGR = 'NGR',
+  GH = 'GH',
+}
+
+export class AmountToWords {
+  private first = [
+    '',
+    'One ',
+    'Two ',
+    'Three ',
+    'Four ',
+    'Five ',
+    'Six ',
+    'Seven ',
+    'Eight ',
+    'Nine ',
+    'Ten ',
+    'Eleven ',
+    'Twelve ',
+    'Thirteen ',
+    'Fourteen ',
+    'Fifteen ',
+    'Sixteen ',
+    'Seventeen ',
+    'Eighteen ',
+    'Nineteen ',
+  ];
+  private tens = ['', '', 'Twenty ', 'Thirty ', 'Forty ', 'Fifty ', 'Sixty ', 'Seventy ', 'Eighty ', 'Ninety '];
+  private numSys: { [currencyCode: string]: string[] } = {
+    usNumSys: ['', 'Hundred ', 'Thousand ', 'Million ', 'Billion ', 'Trillion '],
+    inNumSys: ['', 'Hundred ', 'Thousand ', 'Lakh ', 'Crore '],
+  };
+
+  /**
+   * Ifelere: Naira does not not have an 's' for plural therefore this map is adjusted to have currencies declare plural form
+   */
+  private curCodes: { [countryCode: string]: string[] } = {
+    IND: ['Rupee', 'Paisa', 'Paise', '₹', 'inNumSys', 'Rupees'],
+    USA: ['Dollar', 'Cent', 'Cents', '$', 'usNumSys', 'Dollars'],
+    GBR: ['Pound', 'Pence', 'Pence', '£', 'usNumSys', 'Pounds'],
+    NGR: ['Naira', 'Kobo', 'Kobo', '₦', 'usNumSys', 'Naira'],
+    GH: ['Cedis', 'Pesewas', 'Pesewas', '₵', 'usNumSys', 'Cedis'],
+  };
+
+  public toWords = (amount: string | number, countryCode = 'IND') => {
+    // console.log(num);
+    const numSys = this.numSys[this.getNumSys(countryCode)];
+    const nStr = amount.toString().split('.');
+    // Remove any other characters than numbers
+    const wholeStr = Number(nStr[0].replace(/[^a-z\d\s]+/gi, ''));
+    const decimalStr = nStr.length > 1 ? Number(nStr[1]) : 0;
+    const wholeStrPart =
+      this.getNumSys(countryCode) === 'inNumSys'
+        ? this.convert(wholeStr, numSys).trim()
+        : this.convertInUS(wholeStr, numSys).trim();
+    const decimalPart = this.convert(decimalStr, numSys).trim();
+    let valueInStr =
+      wholeStrPart.length > 0
+        ? `${wholeStrPart} ${this.getCurrencyWhole(countryCode, wholeStr)}`
+        : `Zero ${this.getCurrencyWhole(countryCode, wholeStr)}`;
+    valueInStr =
+      decimalPart.length > 0
+        ? `${valueInStr} And ${decimalPart} ${this.getCurrencyChange(countryCode, decimalStr)}`
+        : valueInStr;
+    // console.log(valueInStr);
+    return valueInStr;
+  };
+
+  private getCurrencyWhole = (countryCode = 'IND', amount = 0) => {
+    // Ifelere: If the amount is more than one use index 5 of curCodes map
+    const index = amount > 1 ? 5 : 0;
+
+    const cur = this.curCodes[countryCode] ? this.curCodes[countryCode][index] : this.curCodes['IND'][index];
+
+    // if (amount > 1) return cur + "s";
+    // else return cur;
+    return cur;
+  };
+
+  private getCurrencyChange = (countryCode: string, amount = 0) => {
+    if (amount > 1) return this.curCodes[countryCode] ? this.curCodes[countryCode][2] : this.curCodes['IND'][2];
+    else return this.curCodes[countryCode] ? this.curCodes[countryCode][1] : this.curCodes['IND'][1];
+  };
+
+  private getCurrencySymbol = (countryCode: string) => {
+    return this.curCodes[countryCode] ? this.curCodes[countryCode][2] : this.curCodes['IND'][2];
+  };
+
+  private getNumSys = (countryCode: string) => {
+    return this.curCodes[countryCode] ? this.curCodes[countryCode][4] : this.curCodes['IND'][4];
+  };
+
+  private convert = (num: number | string, numSys: string[]) => {
+    const numStr = num.toString().split('');
+    const finalStr = [];
+    while (numStr.length > 0) {
+      for (let i = 0; i < numSys.length - 1; ++i) {
+        if (i === 1) finalStr.unshift(this.getUnits(numStr.splice(-1), numSys, i));
+        else finalStr.unshift(this.getUnits(numStr.splice(-2), numSys, i));
+      }
+      if (numStr.length > 0) finalStr.unshift(...numSys.slice(-1));
+    }
+    return finalStr.join('');
+  };
+
+  private convertInUS = (num: number | string, numSys: string[]) => {
+    const numStr = num.toString().split('');
+    const finalStr = [];
+    while (numStr.length > 0) {
+      // console.log(numSys.length);
+      for (let i = 0, l = numSys.length * 2 - 1; i < l; ++i) {
+        // console.log(numStr, i, (i/2)+1);
+        let strValue = '';
+        let curNum = [];
+        if (i === 0) {
+          curNum = numStr.splice(-2);
+          strValue = this.getUnits(curNum, numSys, 0);
+        } else if (i % 2 === 1) {
+          curNum = numStr.splice(-1);
+          strValue = this.getUnits(curNum, numSys, 1);
+        } else {
+          curNum = numStr.splice(-2);
+          strValue = this.getUnits(curNum, numSys, i / 2 + 1);
+        }
+        if (Number(curNum.join('')) !== 0 || numStr.length < 2) finalStr.unshift(strValue);
+      }
+      if (numStr.length > 0) finalStr.unshift(...numSys.slice(-1));
+      else break;
+    }
+    return finalStr.join('');
+  };
+
+  private getUnits = (lastTwo: string[], numSys: string[], place?: number): string => {
+    if (!lastTwo || lastTwo.length === 0) return '';
+    let numInStr = '';
+    if (this.first[Number(lastTwo.join(''))]) numInStr = this.first[Number(lastTwo.join(''))];
+    else numInStr = `${this.tens[Number(lastTwo.shift())]}${this.getUnits(lastTwo.slice(-1), numSys)}`;
+    if ((numInStr && place) || (place && place >= 2)) numInStr = `${numInStr}${numSys[place] ?? ''}`;
+    return numInStr;
+  };
+}
